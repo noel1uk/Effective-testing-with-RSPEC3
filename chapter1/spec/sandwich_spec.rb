7dbf8b2851e421b4a9eff6ac7a00d91d4f9c1503
Noel Vock <noel1uk@gmail.com>
2018-04-17 16:06:14 +0100

    test: example of before hook

--- chapter1/spec/sandwich_spec.rb
+++ chapter1/spec/sandwich_spec.rb
@@ -1,17 +1,15 @@
 Sandwich = Struct.new(:taste, :toppings)
 
 RSpec.describe 'An ideal sandwich' do
+    before { @sandwich = Sandwich.new('delicious', []) }
     it 'is delicious' do
-        sandwich = Sandwich.new('delicious', [])
-
-        taste = sandwich.taste 
-
+        taste = @sandwich.taste 
+        
         expect(taste).to eq('delicious')
     end
     it 'lets me add toppings' do
-        sandwich = Sandwich.new('delicious', [])
-        sandwich.toppings << 'cheese'
-        toppings = sandwich.toppings 
+        @sandwich.toppings << 'cheese'
+        toppings = @sandwich.toppings 
 
         expect(toppings).not_to be_empty
     end
